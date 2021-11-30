dfadf60d8c0afaaf4a54976b985d417f586815e0
Ricardo Zambon <ricardo.zambon@live.com>
2021-11-29 22:18:59 -0300

    Fixed missing audio file from internet

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -1,6 +1,6 @@
 export enum AudioSource {
-  NEW_SEARCH_RESULT = 'https://k003.kiwi6.com/hotlink/bbgk5w2fhz/ping.ogg',
-  SUCCESSFUL_WATCHER_ACCEPT = 'https://k003.kiwi6.com/hotlink/sd6quj2ecs/horn.ogg'
+  NEW_SEARCH_RESULT = '/sounds/alert.wav',
+  SUCCESSFUL_WATCHER_ACCEPT = '/sounds/alert.wav'
 }
 
 export enum TabIndex {
